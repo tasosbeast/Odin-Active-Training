621aaf6d77e534959cec7be1e460e6319d2e2a2a
tasosbeast <antantos@iekdelta.gr>
2024-07-14 20:40:30 +0300

    feat: Add login functionality with password verification

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let login = prompt("Who's there?");
 
+if (login === "Admin") {
+  let password = prompt("Enter the Password");
+  if (password === "TheMaster") {
+    alert("Welcome!");
+  } else if (!password) {
+    alert("Canceled");
+  } else {
+    alert("Wrong Password");
+  }
+} else if (!login) {
+  alert("Canceled");
+} else {
+  alert("i don't know you");
+}
